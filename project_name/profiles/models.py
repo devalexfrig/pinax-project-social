caa7d6402b7cc611bfdf9207ad28417a0a2a132d
Brian Rosner <brosner@gmail.com>
2012-08-06 13:45:07 -0600

    Added User post_save hook to create Profile instances

--- project_name/profiles/models.py
+++ project_name/profiles/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 from django.contrib.auth.models import User
 
@@ -7,3 +9,15 @@ class Profile(models.Model):
     
     user = models.ForeignKey(User, related_name="profile")
     birthdate = models.DateField(null=True, blank=True)
+
+
+@receiver(post_save, sender=User)
+def user_post_save(sender, **kwargs):
+    """
+    Create a Profile instance for all newly created User instances. We only
+    run on user creation to avoid having to check for existence on each call
+    to User.save.
+    """
+    user, created = kwargs["instance"], kwargs["created"]
+    if created:
+        Profile.objects.create(user=user)
