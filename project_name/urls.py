13ec26280cccf6ba2985884f99a7279a3bac058f
Patrick Altman <paltman@gmail.com>
2013-07-05 23:22:31 -0500

    Upgrade requirements

--- project_name/urls.py
+++ project_name/urls.py
@@ -2,16 +2,16 @@ from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.conf.urls.static import static
 
-from django.views.generic.simple import direct_to_template
+from django.views.generic import TemplateView
 
 from django.contrib import admin
 admin.autodiscover()
 
 from .views import SignupView
 
-
-urlpatterns = patterns("",
-    url(r"^$", direct_to_template, {"template": "homepage.html"}, name="home"),
+urlpatterns = patterns(
+    "",
+    url(r"^$", TemplateView.as_view(template_name="homepage.html"), name="home"),
     url(r"^admin/", include(admin.site.urls)),
     url(r"^account/signup/$", SignupView.as_view(), name="account_signup"),
     url(r"^account/", include("account.urls")),
